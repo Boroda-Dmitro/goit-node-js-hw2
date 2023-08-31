32c1339cb7d2ec70ae0aa50e7163ade4fc04821c
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-08-31 09:00:28 +0300

    upload avatar

--- controlers/updateAvatar.js
+++ controlers/updateAvatar.js
@@ -1,30 +1,56 @@
 const { ctrlWrapper } = require("../helpers");
-const path = require("path");
-const fs = require("fs/promises");
+// const path = require("path");
+// const fs = require("fs/promises");
 const { User } = require("../models");
-const { resizeAvatar } = require("../helpers");
+// const { resizeAvatar } = require("../helpers");
 
 
 
-const avatarDir = path.join(__dirname, "../", "public", "avatars");
+// const avatarDir = path.join(__dirname, "../", "public", "avatars");
 
-const updateAvatar = async (req, res) => {
-  const { _id } = req.user;
-  const { path: tempUpload, originalname } = req.file;
-  const filename = `${_id}_${originalname}`;
-  const resultUpload = path.join(avatarDir, filename);
+// const updateAvatar = async (req, res) => {
+//   const { _id } = req.user;
+//   const { path: tempUpload, originalname } = req.file;
+//   const filename = `${_id}_${originalname}`;
+//   const resultUpload = path.join(avatarDir, filename);
 
-  await fs.rename(tempUpload, resultUpload);
+//   await fs.rename(tempUpload, resultUpload);
 
-  await resizeAvatar(resultUpload, 250, 250);
+//   await resizeAvatar(resultUpload, 250, 250);
 
-  const avatarURL = path.join("avatars", filename);
+//   const avatarURL = path.join("avatars", filename);
  
-  await User.findByIdAndUpdate(_id, { avatarURL });
+//   await User.findByIdAndUpdate(_id, { avatarURL });
 
-  res.json({
-    avatarURL,
-  });
+//   res.json({
+//     avatarURL,
+//   });
+// };
+
+const updateAvatar = async (req, res, next) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "File is required" });
+    }
+
+    const { _id } = req.user;
+    const avatarURL = req.file.path;
+
+    const user = await User.findByIdAndUpdate(
+      _id,
+      {
+        avatarURL,
+      },
+      { new: true }
+    );
+
+    return res.json({
+      avatarURL: user.avatarURL,
+    });
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
 };
 
 module.exports = ctrlWrapper(updateAvatar);
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,17 +1,43 @@
+// const multer = require("multer");
+// const path = require("path");
+
+// const tmpDir = path.join(__dirname, "../", "tmp");
+
+// const multerConfig = multer.diskStorage({
+//   destination: tmpDir,
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   }
+// });
+
+// const upload = multer({
+//   storage: multerConfig,
+// });
+
+// module.exports = upload;
+
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
-const path = require("path");
 
-const tmpDir = path.join(__dirname, "../", "tmp");
+const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_SECRET_KEY } =
+  process.env;
 
-const multerConfig = multer.diskStorage({
-  destination: tmpDir,
+cloudinary.config({
+  cloud_name: CLOUDINARY_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_SECRET_KEY,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "avatars",
+  allowedFormats: ["jpg", "png"],
   filename: (req, file, cb) => {
     cb(null, file.originalname);
-  }
+  },
 });
 
-const upload = multer({
-  storage: multerConfig,
-});
+const upload = multer({ storage });
 
 module.exports = upload;
